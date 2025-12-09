7f262cf447d37dab76043eee5b17606d0a28d860
Gustavo Rezende <gustavorezende44@gmail.com>
2025-12-09 11:36:18 -0300

    music added

--- js/gusweb.js
+++ js/gusweb.js
@@ -1,14 +1,15 @@
 const DESTINATION_URL = "easterEgg.html";
 
 const ANIMATION_DURATION = 600; 
-
+const SOUND_FILE = "music/hoshisaga.mp3";
 
 const kanjiLink = document.getElementById('kanji-link');
 const overlay = document.getElementById('transition-overlay');
+const audio = new Audio('music/hoshisaga.mp3')
 
 
 kanjiLink.addEventListener('click', function(event) {
-
+    audio.play();
     event.preventDefault(); 
 
     overlay.classList.add('is-active');
